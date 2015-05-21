4cda208b92ce443bd7741841910866bde8ba5994
Zachary Leighton <zleighton@hvhc.com>
2015-05-21 14:56:31 -0400

    Added ClockModel

---
+++ WinPomodoro/Model/ClockModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WinPomodoro.Model
+{
+    public class ClockModel
+    {
+        private const int TIMER_INTERVAL = 50;
+
+        private DateTime _time;
+
+        public event Action<DateTime> TimeArrived;
+
+        public ClockModel()
+        {
+            Thread thread = new Thread(new ThreadStart(GenerateTimes));
+            thread.IsBackground = true;
+            thread.Priority = ThreadPriority.Normal;
+            thread.Start();
+        }
+
+        public DateTime DateTime
+        {
+            get
+            {
+                return _time;
+            }
+            set
+            {
+                this._time = value;
+                if (TimeArrived != null)
+                {
+                    TimeArrived(DateTime);
+                }
+            }
+        }
+
+        private void GenerateTimes()
+        {
+            while (true)
+            {
+                DateTime = DateTime.Now;
+                Thread.Sleep(TIMER_INTERVAL);
+            }
+        }
+    }
+}
